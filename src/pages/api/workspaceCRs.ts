6ccdcc72d441b3fa4033f2e6e79c1c480158c539
Basak Ekici <sevval.ekici@tum.de>
2022-11-23 17:32:30 +0100

    create and delete workspace cr backend

--- src/pages/api/workspaceCRs.ts
+++ src/pages/api/workspaceCRs.ts
@@ -12,35 +12,63 @@ export type WorkspaceCRData = {
   user: string;
 };
 
+export type WorkspaceCRObj = {
+  kind: string;
+  apiVersion: string;
+  metadata: object;
+};
+
 export default function handler(req: NextApiRequest, res: NextApiResponse<WorkspaceCRData[]>) {
   const kc = new KubeConfig();
   kc.loadFromDefault();
   const k8sApi = kc.makeApiClient(CustomObjectsApi);
   const workspaceCRDataArray: WorkspaceCRData[] = [];
+  if (req.method === 'GET') {
+    k8sApi.listNamespacedCustomObject('theia.cloud', 'v1beta', 'theiacloud', 'workspaces').then((data: any) => {
+      data.body.items &&
+        data.body.items.forEach((each: any) => {
+          const name = each.metadata?.name;
+          const creationTimestamp = each.metadata?.creationTimestamp;
+          const uid = each.metadata?.uid;
+          const appDefinition = each.spec?.appDefinition;
+          const label = each.spec?.label;
+          const storage = each.spec?.storage;
+          const user = each.spec?.user;
 
-  k8sApi.listNamespacedCustomObject('theia.cloud', 'v1beta', 'theiacloud', 'workspaces').then((data: any) => {
-    data.body.items &&
-      data.body.items.forEach((each: any) => {
-        const name = each.metadata?.name;
-        const creationTimestamp = each.metadata?.creationTimestamp;
-        const uid = each.metadata?.uid;
-        const appDefinition = each.spec?.appDefinition;
-        const label = each.spec?.label;
-        const storage = each.spec?.storage;
-        const user = each.spec?.user;
+          const workspaceCRData: WorkspaceCRData = {
+            name,
+            creationTimestamp,
+            uid,
+            appDefinition,
+            label,
+            storage,
+            user,
+          };
 
-        const workspaceCRData: WorkspaceCRData = {
-          name,
-          creationTimestamp,
-          uid,
-          appDefinition,
-          label,
-          storage,
-          user,
-        };
-
-        workspaceCRDataArray.push(workspaceCRData);
+          workspaceCRDataArray.push(workspaceCRData);
+        });
+      res.send(workspaceCRDataArray);
+    });
+  } else if (req.method === 'DELETE') {
+    const toBeDeletedWorkspaceNamesArr = req.body.toBeDeletedWorkspaces
+    toBeDeletedWorkspaceNamesArr.forEach((name: string) => {
+      k8sApi.deleteNamespacedCustomObject('theia.cloud', 'v1beta', 'theiacloud', 'workspaces', name).then((data: any) => {
+        res.status(200).send(data);
       });
-    res.send(workspaceCRDataArray);
-  });
+    })
+  }
+  else if (req.method === 'POST') {
+    const toBeCreatedWorkspaceObj: WorkspaceCRObj = {
+      'kind': 'Workspace',
+      'apiVersion': 'theia.cloud/v1beta',
+      'metadata': {
+        'name': req.body.toBeCreatedWorkspace,
+        'namespace': 'theiacloud'
+      }
+    }
+    
+    k8sApi.createNamespacedCustomObject('theia.cloud', 'v1beta', 'theiacloud', 'workspaces', toBeCreatedWorkspaceObj).then((data: any) => {
+      res.status(200).send(data);
+    });
+  }
 }
