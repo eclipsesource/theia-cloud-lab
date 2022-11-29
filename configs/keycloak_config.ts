60b9d3ba9f1b3a05e27cacd3cefe688a713ccf61
Ilayda Cansin Koc <ilaydacansin@gmail.com>
2022-11-29 19:52:57 +0100

    update config

--- configs/keycloak_config.ts
+++ configs/keycloak_config.ts
@@ -1,12 +1,7 @@
 const keycloakConfig = {
-  appId: '',
-  appName: '',
-  useKeycloak: false,
   keycloakAuthUrl: 'https://keycloak.url/auth/',
   keycloakRealm: 'TheiaCloud',
   keycloakClientId: 'theia-cloud',
-  serviceUrl: '',
-  appDefinition: 'theia-cloud-demo',
 };
 
 export default keycloakConfig;
