1655a138e5dac9d8ffcc93187a74fcf0e7a7044b
Basak Sevval Ekici <sevval.ekici@gmail.com>
2023-02-01 11:08:10 +0100

    admin ws post session changed back

--- src/pages/api/admin/workspaces/session.ts
+++ src/pages/api/admin/workspaces/session.ts
@@ -1,29 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { AdminWorkspaceCRData } from '../../../../../types/AdminWorkspaceCRData';
-// import appConfig from '../../../../../configs/app_config';
+import appConfig from '../../../../../configs/app_config';
 import hostConfig from '../../../../../configs/ws_config';
-// import { TheiaServiceClient } from '../../../../../utils/theiaservice/theiaservice_client';
-import { KubernetesClient } from '../../../../../utils/k8s/k8s_client';
+import { TheiaServiceClient } from '../../../../../utils/theiaservice/theiaservice_client';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // const theiaService = new TheiaServiceClient(req.headers['x-access-token']);
-  const k8s = new KubernetesClient();
+  const theiaService = new TheiaServiceClient(req.headers['x-access-token']);
   if (req.method === 'POST') {
     try {
       await Promise.all(
         req.body.toBeStartedSessions.map(async (workspace: AdminWorkspaceCRData) => {
-          let sessionName = workspace.name + '-session'
-          let sessionUrl = hostConfig.hostUrl + '/' + workspace.uid + '/'
-          await k8s.createSession(sessionName, workspace.name, workspace.appDefinition, sessionUrl, workspace.user);
-          // Theia Cloud Service is checking some keycloak configurations, our roles are maybe not recognized by your service api. 
-          // After arranging that you can change it to theia api back again.
-          // await theiaService.createSessionWithExistingWorkspace(
-          //   appConfig.appId,
-          //   workspace.user,
-          //   workspace.name,
-          //   workspace.appDefinition
-          // );
+          await theiaService.createSessionWithExistingWorkspace(
+            appConfig.appId,
+            workspace.user,
+            workspace.name,
+            workspace.appDefinition
+          );
         })
       );
       return res.status(201).send({});
